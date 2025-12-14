ca6b4a12f101e0e3ac79d2fe817cf6e3bd3a0f5c
spiyse <daniels@spiyse.dev>
2025-12-14 15:38:38 +0200

    made files where the GUI will be

---
+++ marqet_findr/ui/app.py
@@ -0,0 +1,5 @@
+# Nothing too see here yet ~~
+
+# Will be uising customtkinter for the UI
+
+# Will make the UI after im done getting the base info of listings ~ 
---
+++ marqet_findr/ui/widgets.py
@@ -0,0 +1,1 @@
+# This will have classes for custom GUI elements :>
